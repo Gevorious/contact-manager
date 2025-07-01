27349b021f1270a2dae6e20c2de3e33bb5ef4b78
Gevorious <gevorgpahlevanyan@gmail.com>
2025-07-01 23:57:37 +0400

    added env with base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3001";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const fetchContacts = async () => {
   const response = await fetch(`${API_BASE_URL}/contacts`);
